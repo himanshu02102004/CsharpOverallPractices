c93b3b883e339084a85c5b91bcd058480fe16329
Nimap <Nimap@DESKTOP-EOO2UFO>
2025-06-19 09:50:32 +0530

    mvc

---
+++ Logical_ques/Logical_ques/NOT_REPEAT_CHAR.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_ques
+{
+    class NOT_REPEAT_CHAR
+    {
+
+
+        /// <summary>
+        /// FIRST APPROACH  USING 
+        /// </summary>
+        /
+
+        public static void NOTREPEAT(string[] args)
+        {
+            string str = "himah";
+            bool isnotrepeat;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                isnotrepeat = false;
+
+                for (int j = 0; j < str.Length; j++)
+                {
+                    if (i != j && str[i] == str[j])
+                    {
+
+                        isnotrepeat = true;
+                        break;
+                    }
+
+                }
+
+                if (!isnotrepeat)
+                {
+                    Console.WriteLine(str[i]);
+                }
+            }
+
+
+
+        }
+    }
+}
+
