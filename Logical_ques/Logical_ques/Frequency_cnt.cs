8c378b0be1c4767b0f4a1eb069655b7868e14bc5
himanshu02102004 <himanshu2104mishra@gmail.com>
2025-07-24 23:04:42 +0530

    aded

--- Logical_ques/Logical_ques/Frequency_cnt.cs
+++ Logical_ques/Logical_ques/Frequency_cnt.cs
@@ -70,3 +70,49 @@ public class HelloWorld
     }
 }
 }
+
+
+
+
+
+?/////////////without inbuild function
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+
+    public static bool ana(string s1, string s2)
+    {
+        if (s1 == null || s2 == null) return false;
+
+        s1 = s1.ToLower();
+        s2 = s2.ToLower();
+
+        if (s1.Length != s2.Length) return false;
+        int[] f1 = new int[26];
+        int[] f2 = new int[26];
+        for (int i = 0; i < s1.Length; i++)
+        {
+            f1[s1[i] - 'a']++;
+            f2[s2[i] - 'a']++;
+
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (f1.Length != f2.Length) return false;
+        }
+        return true;
+
+
+    }
+    public static void Main(string[] args)
+    {
+        string s1 = "len";
+        string s2 = "snel";
+        Console.WriteLine(ana(s1, s2));
+    }
+}
---
+++ Logical_ques/Logical_ques/Longest_Substring.cs
@@ -0,0 +1,43 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Logical_ques
+//{
+//    class Longest_Substring
+//    {
+//    }
+
+
+//    // Online C# Editor for free
+//// Write, Edit and Run your C# code using C# Online Compiler
+
+//using System;
+
+//public class HelloWorld
+//    {
+//        public static void Main(string[] args)
+//        {
+//            string s1 = "aaabbcccddddeee";
+//            int[] f1 = new int[26];
+//            for (int i = 0; i < s1.Length; i++)
+//            {
+//                f1[s1[i] - 'a']++;
+//            }
+//            int max = 0;
+//            char maxchar = 'a';
+//            for (int j = 0; j < 26; j++)
+//            {
+//                if (max < f1[j])
+//                {
+//                    max = f1[j];
+//                    maxchar = (char)(j + 'a');
+//                }
+//            }
+//            Console.WriteLine("max " + max);
+//            Console.WriteLine("freqchar " + maxchar);
+//        }
+//    }
+//}
